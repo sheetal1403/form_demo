3876aeee17dd175445343b4fd6b8db990e0de87f
Sheetal R <sheetalraghupathi14@gmail.com>
2020-10-08 11:20:26 +0530

    Add form header component

---
+++ src/Form/Form.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import FormHeader from '../FormHeader/FormHeader';
+
+class Form extends Component{
+
+
+
+    render(){
+        return (
+            <FormHeader/>
+        )
+    }
+}
+
+export default Form
---
+++ src/FormHeader/FormHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FormHeader() {
+    return (
+        <div>
+            <input type="text" placeholder="Form title"/>
+            <input type="text" placeholder="Form description"/>
+        </div>
+    )
+}
+
+export default FormHeader
