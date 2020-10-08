2847a21ced10c0913fb12a52592dd89dc910c020
Sheetal R <sheetalraghupathi14@gmail.com>
2020-10-08 12:32:28 +0530

    Add multiple choice, max 5, on clicking 'add'

--- src/AnswerInputs/MultipleChoice/MultipleChoice.js
+++ src/AnswerInputs/MultipleChoice/MultipleChoice.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function MultipleChoice() {
+
+    const [no_of_options, setno_of_options] = useState(1);
+
+    const increment = () => {
+        setno_of_options((prevState) => prevState + 1)
+    }
+
     return (
         <div>
-            <input type="radio" />
-            <input type="radio" />
-            <input type="radio" />
+            {Array(no_of_options).fill().map((_,index) => {
+                return (
+                   <React.Fragment key={index}>
+                       <input type="radio" id="option1" name="option1"/>
+                       <label htmlFor="option1">Option1</label>
+                       <br/>
+                   </React.Fragment>
+                    
+                )
+            }
+                
+            )}
+            
+            <p onClick={increment}>{no_of_options < 5 ? 'Add option' : null}</p>
         </div>
     )
 }
